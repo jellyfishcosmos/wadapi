1b60e188725feaa7e31668cfb49003ccde40fe11
Sam Taylor <s.taylor2099@outlook.com>
2023-12-18 22:59:24 +0000

    ADDED JWT

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+
 
 const router = express.Router(); // eslint-disable-line
 
@@ -12,33 +14,21 @@ router.get('/', asyncHandler(async (req, res) => {
 
 // register(Create)/Authenticate User
 router.post('/', asyncHandler(async (req, res) => {
-    if (req.query.action === 'register') {
-      const user = new User(req.body);
-  
-      try {
-        // Attempt to save the user to the database
-        await user.save();
-        res.status(201).json({
-          code: 201,
-          msg: 'Successfully created a new user.',
-        });
-      } catch (error) {
-        // Handle Mongoose validation error
-        if (error.name === 'ValidationError') {
-          const errors = Object.values(error.errors).map((err) => err.message);
-          return res.status(400).json({ code: 400, msg: 'Validation error', errors });
-        }
-        // Handle other errors
-        console.error('User registration error:', error);
-        res.status(500).json({ code: 500, msg: 'Internal Server Error' });
-    }}
- else {
-    const user = await User.findOne(req.body);
-    if (!user) {
-      return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-    } else {
-      return res.status(200).json({ code: 200, msg: 'Authentication Successful', token: 'TEMPORARY_TOKEN' });
-}}}));
+  try {
+      if (!req.body.username || !req.body.password) {
+          return res.status(400).json({ success: false, msg: 'Username and password are required.' });
+      }
+      if (req.query.action === 'register') {
+          await registerUser(req, res);
+      } else {
+          await authenticateUser(req, res);
+      }
+  } catch (error) {
+      // Log the error and return a generic error message
+      console.error(error);
+      res.status(500).json({ success: false, msg: 'Internal server error.' });
+  }
+}));
 
 // Update a user
 router.put('/:id', asyncHandler(async (req, res) => {
@@ -52,4 +42,25 @@ router.put('/:id', asyncHandler(async (req, res) => {
       res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
   }));
+
+  async function registerUser(req, res) {
+    // Add input validation logic here
+    await User.create(req.body);
+    res.status(201).json({ success: true, msg: 'User successfully created.' });
+}
+
+async function authenticateUser(req, res) {
+    const user = await User.findByUserName(req.body.username);
+    if (!user) {
+        return res.status(401).json({ success: false, msg: 'Authentication failed. User not found.' });
+    }
+
+    const isMatch = await user.comparePassword(req.body.password);
+    if (isMatch) {
+        const token = jwt.sign({ username: user.username }, process.env.SECRET);
+        res.status(200).json({ success: true, token: 'BEARER ' + token });
+    } else {
+        res.status(401).json({ success: false, msg: 'Wrong password.' });
+    }
+}
 export default router;
